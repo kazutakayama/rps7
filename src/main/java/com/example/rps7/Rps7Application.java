f313fcabddcfc301c9d977704292374431f44033
kazutaka <k89z10@yahoo.co.jp>
2025-06-11 15:13:54 +0900

    first commit

---
+++ src/main/java/com/example/rps7/Rps7Application.java
@@ -0,0 +1,13 @@
+package com.example.rps7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rps7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rps7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rps7/Rps7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rps7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rps7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
